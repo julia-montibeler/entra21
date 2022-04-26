776f7d58cb96094224f718e97c556be1ebf5fd88
Júlia de Amaral Montibeler <jamontibeler@furb.br>
2022-04-26 18:04:51 -0300

    logica

---
+++ log2504/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    //comentário de linha
+    /* comentário de bloco */
+
+    static int v1 = 10; //estabelecer variavel como static quando usar public static void
+    static double v2 = 10.5;
+    char v3 = 'a';
+    String v4 = "abc";
+    boolean v5 = true;
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        v1 += 5;
+        System.out.println(v1);
+        System.out.println(--v1); //operador unário antes da variável realiza primeiro a operação.
+
+        System.out.println(v1==v2);
+
+
+
+
+
+
+
+    }
+    //private
+    //protected
+
+
+}
