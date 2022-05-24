c7511f9f23fe3ca17303860d66c55a0c0a20af1f
Júlia de Amaral Montibeler <jamontibeler@furb.br>
2022-05-23 21:49:19 -0300

    23/05

---
+++ POO/src/classes/HotDog.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class HotDog extends Lanche{
+    public HotDog() {
+        this.adicionarIngrediente("Pão");
+        this.adicionarIngrediente("Salsicha");
+        this.adicionarIngrediente("Batata Palha");
+        this.adicionarIngrediente("Vinagrete");
+        this.adicionarIngrediente("Queijo Cheddar");
+    }
+}
